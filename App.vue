30ff758569b3d2efe4e8ae368e2937cdb12c5c02
Lin <47935197+linlei1120@users.noreply.github.com>
2025-06-18 16:10:13 +0800

    微信API转发分享DEMO

--- App.vue
+++ App.vue
@@ -7,8 +7,20 @@ import { onLaunch } from "@dcloudio/uni-app";
 
 const { proxy } = getCurrentInstance();
 
-onLaunch(() => {
+onLaunch((option) => {
+	console.log("onLaunch", option);
 	initApp();
+	wx.getShareInfo({
+		shareTicket: option.shareTicket,
+		success: (res) => {
+			console.log("获取转发分享信息成功", res);
+			//参考响应参数
+			//cloudID: "93_Vu1Jk7EEdqubuSJSbsKzMifIlEAUuoBsyKvtJIJyKZ_ZdhbcbVhHO4L66Js" //敏感数据对应的云 ID，开通云开发的小程序才会返回
+			// encryptedData: "9zbJiTUzDnmXktoSN+6a0XgKVL5RI0RBgbXbxBTFCNGtqvUd9VYi/j4LbuO93nAsFdZB8wA6G8+lF7Z8rM4D/lbQbdIlfNUXOfZNazX+t7hW9skDJ4nIpq6O6g+eneXLR683hvUCMigN03fHMEG1Bg=="
+			// errMsg: "getShareInfo:ok"
+			// iv: "OoO51sAIOpNPSFDDtv9KxA=="
+		}
+	});
 });
 
 // 初始化应用
